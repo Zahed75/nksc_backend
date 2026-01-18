8ca4cc80b72a34ddc4048a56c74d80461fba10cb
“Zahed75” <zahedhasan.cs@gmail.com>
2026-01-18 16:06:51 +0600

    staff done

--- nksc_backend/urls.py
+++ nksc_backend/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     path("api/user-management/", include("user_management.urls")),
     path("api/news/", include("news.urls")),
     path('api/gallery/', include('media_stuff.urls')),
+    path('api/staff/', include('staff.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
